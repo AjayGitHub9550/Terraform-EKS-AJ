6cc262a1b281845ac0de4f1e4a6900f2399369e6
Ajay Y <ajay.yerukula@outlook.com>
2026-03-18 16:37:51 +0530

    NAT $ disk add

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -19,5 +19,6 @@ resource "aws_eks_node_group" "node" {
     min_size     = 1
   }
 
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.small"]
+  disk_size = 30
 }
---
+++ modules/nat/main.tf
@@ -0,0 +1,28 @@
+resource "aws_eip" "nat" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = var.public_subnet_id
+
+  tags = {
+    Name = "eks-nat-gateway"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_route" "private_nat" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnet_ids)
+  subnet_id      = var.private_subnet_ids[count.index]
+  route_table_id = aws_route_table.private.id
+}
