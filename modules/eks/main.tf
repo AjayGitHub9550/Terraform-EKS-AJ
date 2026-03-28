a52deb2ffa2bac11d9cca4b25def7d2772a3d50f
Ajay Y <ajay.yerukula@outlook.com>
2026-03-28 22:38:21 +0530

    increase the pods

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -14,9 +14,9 @@ resource "aws_eks_node_group" "node" {
   subnet_ids      = var.private_subnets
 
   scaling_config {
-    desired_size = 0
-    max_size     = 1
-    min_size     = 0
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
   }
 
   instance_types = ["t3.small"]
